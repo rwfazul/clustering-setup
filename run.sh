571d10114e884af89b7efa30d9d9d7f74a5677a3
rwfazul <rwfazul@inf.ufsm.br>
2018-11-18 00:11:07 -0200

    startup

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
 docker-compose up -d --scale app=2 --scale frontend=2
+
+# View logs: $ docker-compose logs -f -t 
+# Shutdown: $ docker-compose down 
+
+# Restart specific service/container: $ docker-compose restart <service_name> 
+# View logs of specific service/container: $ docker-compose logs -f -t <service_name>
+# ...
