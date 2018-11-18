1476ad5f8b46afbc763b0f3fa391c946844df22d
rwfazul <rwfazul@inf.ufsm.br>
2018-11-17 23:33:12 -0200

    static files served by nginx

---
+++ web/js/playlists/ajax.js
@@ -0,0 +1,28 @@
+const API = 'http://localhost/api';
+const playlistAPI = `${API}/playlists`;
+
+function doAjax(url, method, data, success, error) {
+	$.ajax({
+	    url: url,
+	   	method: method,
+	    data: data,
+	    success: success,
+	    error: error
+	});
+}
+
+function findAllPlaylists(cbSuccess, cbError) {
+	doAjax(playlistAPI, 'GET', '', cbSuccess, cbError);
+}
+
+function insertPlaylist(playlist, cbSuccess, cbError) {
+	doAjax(playlistAPI, 'POST', playlist, cbSuccess, cbError);
+}
+
+function updatePlaylist(id, playlist, cbSuccess, cbError) {
+	doAjax(`${playlistAPI}/${id}`, 'PUT', playlist, cbSuccess, cbError);
+}
+
+function removePlaylist(id, cbSuccess, cbError) {
+	doAjax(`${playlistAPI}/${id}`, 'DELETE', '', cbSuccess, cbError);
+}
